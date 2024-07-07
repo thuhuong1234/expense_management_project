a37a42167ad26756a046ad5b4678455bfd24bf59
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2024-07-08 00:54:20 +0700

    :chicken: Implement todos page UI

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -4,8 +4,65 @@ import DashboardLayout from "../layouts/DashboardLayout.vue";
 
 <template>
   <DashboardLayout>
-    <div class="todo-page">Todos Page</div>
+    <div class="todo-page">
+      <div class="todos-page">
+        <form class="todo-form">
+          <input class="input" type="text" placeholder="Enter title" />
+          <input
+            class="input"
+            type="text"
+            placeholder="Enter amount of money"
+          />
+          <button class="button" type="submit">Save</button>
+        </form>
+        <div class="todo-list">
+          <div class="todo-item">
+            <span class="no">1</span>
+            <span class="title">Buy a new car</span>
+            <span class="money">$50,000</span>
+          </div>
+          <div class="todo-item">
+            <span class="no">2</span>
+            <span class="title">Buy a new house</span>
+            <span class="money">$150,000</span>
+          </div>
+          <div class="todo-item">
+            <span class="no">3</span>
+            <span class="title">Buy a new laptop</span>
+            <span class="money">$150,000</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </DashboardLayout>
 </template>
 
-<style scoped></style>
+<style scoped>
+.todo-form {
+  display: flex;
+  gap: 16px;
+  margin-bottom: 32px;
+}
+.todo-form > .input {
+  width: 30%;
+  padding: 8px;
+}
+.todo-form > .button {
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  background-color: #007bff;
+  color: #fff;
+  cursor: pointer;
+}
+.todo-item {
+  display: flex;
+  padding: 8px;
+  gap: 8px;
+  border-bottom: 1px solid #ccc;
+}
+.todo-item > .title {
+  flex: 1;
+  font-weight: bold;
+}
+</style>
