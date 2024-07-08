0093be7e7cf67390a1d3b61ed2b68243cace84b8
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2024-07-08 14:33:41 +0700

    :sunflower: Updated  more todo information  about category

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -3,16 +3,27 @@ import DashboardLayout from "../layouts/DashboardLayout.vue";
 import { onMounted, ref, reactive } from "vue";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const todoList = ref([]);
+const categoryList = ref([]);
 
 const todoForm = reactive({
   name: undefined,
   amountOfMoney: undefined,
+  categoryId: undefined,
 });
 const handleFetchTodos = async () => {
   try {
     const response = await fetch(`${API_BASE_URL}/todos`);
     if (response.ok) {
       todoList.value = await response.json();
+      await handleFetchCategories();
+      // Add category names to todos
+      const updatedTodos = todoList.value.map((todo) => {
+        const category = categoryList.value.find(
+          (category) => category.id === todo.categoryId
+        );
+        return { ...todo, categoryName: category ? category.name : "Unknown" };
+      });
+      todoList.value = updatedTodos;
     }
   } catch (error) {
     console.log(error);
@@ -93,7 +104,16 @@ const handleSubmit = async () => {
     todoForm.amountOfMoney = undefined;
   }
 };
-
+const handleFetchCategories = async () => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/categories`);
+    if (response.ok) {
+      categoryList.value = await response.json();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 onMounted(() => {
   handleFetchTodos();
 });
@@ -116,6 +136,18 @@ onMounted(() => {
             placeholder="Enter amount of money"
             v-model="todoForm.amountOfMoney"
           />
+          <div class="dropdown">
+            <select class="dropbtn input" v-model="todoForm.category">
+              <option disabled value="">Category</option>
+              <option
+                v-for="category in categoryList"
+                :key="category.id"
+                :value="category.id"
+              >
+                {{ category.name }}
+              </option>
+            </select>
+          </div>
           <button class="button" type="submit">Save</button>
         </form>
         <div class="todo-list">
@@ -127,12 +159,14 @@ onMounted(() => {
           >
             <span class="no">{{ index + 1 }}</span>
             <span class="title">{{ todo.name }}</span>
+            <span class="title">{{ todo.categoryName }}</span>
             <span class="money">{{
               new Intl.NumberFormat("vi-VN", {
                 style: "currency",
                 currency: "VND",
               }).format(todo.amountOfMoney)
             }}</span>
+
             <button class="button" @click="handleEditTodo(todo)">Edit</button>
             <button
               class="button"
@@ -184,5 +218,43 @@ onMounted(() => {
   background-color: #007bff;
   color: #fff;
   cursor: pointer;
+}
+.dropdown {
+  position: relative;
+  display: inline-block;
+  width: 15%;
+  padding: 8px;
+}
+.dropbtn {
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: white;
+  cursor: pointer;
+  font-size: 16px;
+  text-align: left;
+}
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+}
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+.dropdown-content a:hover {
+  color: black;
+  background-color: #f1f1f1;
+  display: block;
+}
+.dropdown:hover .dropdown-content {
+  display: block;
 }
 </style>
