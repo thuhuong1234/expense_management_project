afba7201a494af138a276c3ead9e4d9cf94c3867
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2025-04-11 23:28:48 +0700

    :+1: Import argon-dashboard

--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap";
 import { createPinia } from "pinia";
+import "./assets/css/nucleo-icons.css";
+import "./assets/css/nucleo-svg.css";
+import ArgonDashboard from "@/argon-dashboard";
 
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
-
+app.use(ArgonDashboard);
 app.mount("#app");
