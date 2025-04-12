08bda0d8d644aecf40ad87a2f0d7db10ecffdbe8
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2025-04-13 00:23:20 +0700

    :+1: Improved login page

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,26 +1,15 @@
 <template>
-    <div class="container top-0 position-sticky z-index-sticky">
-        <div class="row">
-            <div class="col-12">
-                <navbar is-blur="blur border-radius-lg my-3 py-2 start-0 end-0 mx-4 shadow"
-                    btn-background="bg-gradient-success" :dark-mode="true" />
-            </div>
-        </div>
-    </div>
     <main class="main-content main-content-bg mt-0">
         <div class="page-header min-vh-100 ">
-            <span class="mask bg-gradient-dark opacity-6"></span>
             <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-4 col-md-7 mt-2">
-                        <div class="card border-0 mb-0 ">
-                            <div class="card-header bg-transparent">
-                                <h5 class="text-dark text-center mt-2 mb-3">Login</h5>
+                <div class="row">
+                    <div class="mx-auto col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0">
+                        <div class="card card-plain ">
+                            <div class="pb-0 card-header text-start">
+                                <h4 class="font-weight-bolder">Đăng nhập </h4>
+                                <p class="mb-0">Nhập đầy đủ thống tin đăng nhập</p>
                             </div>
-                            <div class="card-body pt-0 ">
-                                <div class="text-center text-muted mb-4 ">
-                                    <small>Enter your login information to gain access!</small>
-                                </div>
+                            <div class="card-body">
                                 <form role="form" class="text-start" @submit.prevent="onSubmit">
                                     <div class="mb-3">
                                         <argon-input name="email" id="email" type="email" placeholder="Email"
@@ -28,25 +17,40 @@
                                     </div>
                                     <div class="mb-3">
                                         <argon-input name="password" id="password" type="password"
-                                            placeholder="Password" :api-error="apiErrors.password" />
+                                            placeholder="Mật khẩu" :api-error="apiErrors.password" />
                                     </div>
                                     <div class=" d-flex justify-content-between fs-6">
-                                        <div class=" login">
-                                            Don't have an account?
+                                        <div class="login">
+                                            Chưa có tài khoản?
                                             <router-link :to="{ name: 'register' }" class="register fs-6"
                                                 style="text-decoration: none;">
-                                                Register</router-link :to="{ name: 'register' }">
+                                                Đăng ký</router-link :to="{ name: 'register' }">
                                         </div>
                                         <router-link :to="{ name: 'forgot-password' }" class="forgot-password fs-6"
-                                            style="text-decoration: none;">Forgot
-                                            password?</router-link :to="{ name: 'forgot-password' }">
+                                            style="text-decoration: none;">Quên mật khẩu?
+                                        </router-link :to="{ name: 'forgot-password' }">
                                     </div>
                                     <div class="text-center">
-                                        <argon-button color="success" type="submit" variant="gradient" full-width
-                                            class="my-4 mb-2 text-white ">Login</argon-button>
+                                        <argon-button color="" type="submit" variant="gradient" full-width
+                                            class="my-4 mb-2 text-white btn-login ">Đăng nhập</argon-button>
                                     </div>
                                 </form>
                             </div>
+                        </div>
+                    </div>
+                    <div
+                        class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
+                        <div
+                            class=" img-login position-relative h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden">
+                            <span class="mask bg-gradient-danger opacity-5"></span>
+
+                            <h5 class="mt-5 text-white font-weight-bolder position-relative">
+                                "Attention is the new currency"
+                            </h5>
+                            <p class="text-white position-relative">
+                                The more effortless the writing looks, the more effort the
+                                writer actually put into the process.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -100,16 +104,7 @@ const onSubmit = handleSubmit(async (values) => {
 }); 
 </script>
 <style scoped>
-:deep(.bg-gradient-success) {
-    background: linear-gradient(87deg, #2dce89 0, #2dcecc 100%) !important;
-}
-
-:deep(.bg-gradient-dark) {
-    background: linear-gradient(87deg, #212429 0, #212429 100%) !important;
-}
-
 .page-header {
-    background-image: url("https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-basic.jpg");
     background-size: cover;
     background-position: center;
     display: flex;
@@ -123,11 +118,40 @@ const onSubmit = handleSubmit(async (values) => {
     left: 0;
     width: 100%;
     height: 100%;
-    background: rgba(0, 0, 0, 0.4);
     z-index: 0;
 }
 
 .fs-6 {
     font-size: 14px !important;
+}
+
+.main-content {
+    font-family: 'Lora' !important;
+}
+
+.btn-login {
+    background-color: #ee3672 !important;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    font-weight: 500;
+    font-size: 16px;
+    font-family: 'Lora' !important;
+}
+
+.img-login {
+    background-position: center center;
+    background-size: cover;
+    background-image: url('@/assets/img/img-1.jpg');
+}
+
+.card .card-body {
+    font-family: 'Lora' !important;
+    color: #3e3e46 !important;
+}
+
+.card-header,
+h4 {
+    background-color: #f8f9fa !important;
+    color: #3e3e46 !important;
 }
 </style>
